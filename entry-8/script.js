6aa170afe92f9248edb15b04b1c29e069a828b08
cameronS2000 <70330251+cameronS2000@users.noreply.github.com>
2023-11-06 19:23:00 -0500

    entry-8

---
+++ entry-8/script.js
@@ -0,0 +1,62 @@
+let isMouseDown = false;
+
+const gridContainer1 = document.getElementById("grid1");
+for (let i = 0; i < 15; i++) {
+  for (let j = 0; j < 20; j++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.addEventListener("mousedown", () => {
+      isMouseDown = true;
+      box.style.backgroundColor = "black";
+    });
+    box.addEventListener("mouseover", () => {
+      if (isMouseDown) {
+        box.style.backgroundColor = "black";
+      }
+    });
+    box.addEventListener("mouseup", () => {
+      isMouseDown = false;
+    });
+    gridContainer1.appendChild(box);
+  }
+}
+const gridContainer2 = document.getElementById("grid3");
+for (let i = 0; i < 15; i++) {
+  for (let j = 0; j < 20; j++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.addEventListener("mousedown", () => {
+      isMouseDown = true;
+      box.style.backgroundColor = "black";
+    });
+    box.addEventListener("mouseover", () => {
+      if (isMouseDown) {
+        box.style.backgroundColor = "black";
+      }
+    });
+    box.addEventListener("mouseup", () => {
+      isMouseDown = false;
+    });
+    gridContainer2.appendChild(box);
+  }
+}
+const gridContainer3 = document.getElementById("grid2");
+for (let i = 0; i < 15; i++) {
+  for (let j = 0; j < 20; j++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.addEventListener("mousedown", () => {
+      isMouseDown = true;
+      box.style.backgroundColor = "black";
+    });
+    box.addEventListener("mouseover", () => {
+      if (isMouseDown) {
+        box.style.backgroundColor = "black";
+      }
+    });
+    box.addEventListener("mouseup", () => {
+      isMouseDown = false;
+    });
+    gridContainer3.appendChild(box);
+  }
+}
